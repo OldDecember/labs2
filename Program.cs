58991718f31c119d82d0c019c13508f337c6fefe
OldDecember <beel1998@mail.ru>
2026-07-29 11:42:31 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+// Program.cs
+using System;
+
+namespace AnimalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в систему управления животными!");
+        }
+    }
+}
